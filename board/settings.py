f63a04059bcef525f3b298361c1415df384d5ec5
m-root <sbginter@gmail.com>
2017-12-27 09:07:26 +0300

    Added accounts to settings

--- board/settings.py
+++ board/settings.py
@@ -37,10 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts'
     'boards',
     # 'static',
     'widget_tweaks',
-    # 'django-bootstrap3'
+
 ]
 
 MIDDLEWARE = [
