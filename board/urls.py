b69b2178bc55fdd4b2ad86bd7e3cf28ca10e43ff
m-root <sbginter@gmail.com>
2017-12-28 21:50:44 +0300

    Added Login & Logout

--- board/urls.py
+++ board/urls.py
@@ -18,12 +18,17 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import static
 from accounts import views as accounts_views
-from board import settings
+from django.contrib.auth import views as auth_views
+
+from accounts import views as accounts_views
+
 from boards import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^signup/$', accounts_views.signup, name='signup'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name = 'login.html'), name='login'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='board_topics_new'),
     # url(r'^boards/(?P<pk>\d+)/$', views.view_topics, name='board_view'),
