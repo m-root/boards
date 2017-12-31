208f50684f2a288854100239d42748fe6728d20f
m-root <sbginter@gmail.com>
2017-12-31 23:26:27 +0300

    Minor cleanup of the code.

--- accounts/views.py
+++ accounts/views.py
@@ -15,12 +15,8 @@ def signup(request):
     return render(request, 'signup.html', {'form':form})
 
 def login(request):
-    # if request.method == 'POST':
-    #     form = UserCreationForm(request.POST)
-    #     if form.is_valid():
-    #         user = form.save()
-    #         auth_login(request, user)
-    #         return redirect('home')
-    # else:
     form = LoginForm()
     return render(request, 'login.html', {'form':form})
+
+
+
